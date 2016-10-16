6535052b6f0fddef339fb1e4fd1537d6b1ba03bf
Sunil Shrestha <sunil@iwa.fi>
2016-10-16 21:00:19 +0300

    Integrate cordova-sqlite-database as fallback db

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import { Adapter } from 'ember-pouch';
 import PouchDB from 'pouchdb';
+import PouchAdapterCordovaSqlite from 'npm:pouchdb-adapter-cordova-sqlite';
 import config from 'tidy-closet/config/environment';
 import Ember from 'ember';
 
@@ -7,10 +8,14 @@ const { assert, isEmpty } = Ember;
 
 function createDb() {
   let localDb = config.emberPouch.localDb;
-
+  PouchDB.plugin(PouchAdapterCordovaSqlite);
   assert('emberPouch.localDb must be set', !isEmpty(localDb));
 
-  let db = new PouchDB(localDb);
+  let db = new PouchDB(localDb, {
+    adapter: 'cordova-sqlite',
+    iosDatabaseLocation: 'Library',
+    androidDatabaseImplementation: 2
+  });
 
   if (config.emberPouch.remoteDb) {
     let remoteDb = new PouchDB(config.emberPouch.remoteDb);
